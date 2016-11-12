8e3e0f4e0674d0f089c051ca3b3a86be6334d29a
T. Ngtk <ngtkt0909@gmail.com>
2016-11-12 13:14:45 +0900

    mouse-wheel-mode, menu-bar-mode, tool-bar-mode is evaluated when window-system

--- .emacs.el
+++ .emacs.el
@@ -48,7 +48,6 @@
 (setq frame-title-format                  ;; フレームのタイトル指定
       (concat "%b - emacs" emacs-version "@" system-name))
 
-(mouse-wheel-mode t)                      ;; ホイールマウスを有効にする
 (global-font-lock-mode t)                 ;; 文字の色つけ
 (auto-compression-mode t)                 ;; 日本語infoの文字化け防止
 (transient-mark-mode t)                   ;; リージョンをハイライト表示
@@ -59,8 +58,9 @@
 (which-function-mode t)                   ;; 現在の間数名をモードラインに表示
 (size-indication-mode t)                  ;; ファイルサイズを表示
 
-(if window-system (menu-bar-mode 1) (menu-bar-mode -1))  ;; -nw で起動したときにはメニューバーを消去
-(if window-system (tool-bar-mode 1) (tool-bar-mode -1))  ;; -nw で起動したときにはツールバーを消去
+(when window-system (mouse-wheel-mode t)) ;; ホイールマウスを有効にする
+(when window-system (menu-bar-mode -1))   ;; メニューバーを非表示
+(when window-system (tool-bar-mode -1))   ;; ツールバーを非表示
 
 (setq inhibit-startup-screen t)           ;; 起動時の*GNU Emacs*バッファの消去
 (setq inhibit-startup-message t)          ;; 起動時のメッセージを非表示
